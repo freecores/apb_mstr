14104fd052469caa752d8ad7d86dd24b9963c6e0
eyalhoc <eyalhoc@776818de-ecd0-4841-a7a9-0ea8b2305e4c>
2011-04-29 15:42:08 +0000

    create prgen rand

--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-../../../robust ../src/base/apb_master.v -od out -I ../src/gen -list list.txt -listpath -header
+../../../robust ../src/base/apb_master.v -od out -I ../src/gen -list list.txt -listpath -header ${@}
 
 echo Completed RobustVerilog APB master run - results in run/out/
